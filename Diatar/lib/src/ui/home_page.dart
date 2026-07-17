fc2be3ecf11e9b2d79c6180cfad99db9915004de
Vörös László <theprogrambear@gmail.com>
2026-07-17 14:40:16 +0200

    Napi lelki batyu

--- Diatar/lib/src/ui/home_page.dart
+++ Diatar/lib/src/ui/home_page.dart
@@ -566,21 +566,21 @@ class _DiatarHomePageState extends State<DiatarHomePage> {
             onPressed: () => _openZsolozsmaDialog(context),
             icon: const Icon(Icons.menu_book_outlined),
           ),
-          /* IconButton(
+          IconButton(
             tooltip: l10n.batyuTooltip,
             onPressed: () => _openBatyuDialog(context),
             icon: const Icon(Icons.auto_stories_outlined),
-          ), */ 
+          ), 
           IconButton(
             tooltip: l10n.downloadBooksTooltip,
             onPressed: () => _openDownloadDialog(context),
             icon: const Icon(Icons.download_for_offline_outlined),
           ),
-          IconButton(
+          /* IconButton(
             tooltip: l10n.refreshTooltip,
             onPressed: controller.reloadBooks,
             icon: const Icon(Icons.refresh),
-          ),
+          ),*/
         ],
       ),
       body: AnimatedBuilder(
@@ -845,7 +845,7 @@ class _DiatarHomePageState extends State<DiatarHomePage> {
                     ? const Color(0xFF1976D2)
                     : Theme.of(context).colorScheme.onSurfaceVariant,
               ),
-              const SizedBox(width: 8),*/ 
+              const SizedBox(width: 8),*/
               _actionIconButton(
                 context,
                 icon: controller.settings.projectionLocked
--- HIDDEN.md
+++ HIDDEN.md
@@ -2,3 +2,4 @@ cast_service.dart 33 false && (CAST)
 home_page.dart 905-943 /* */ (Transzponálás)
 home_page.dart 569-573 /* */ Napi lelki batyu
 home_page.dart 832-848 /* */ Fényképnézet
+home_page.dart 579-583 /* */ reloadbooks (igazából majd ki is lehet venni)
