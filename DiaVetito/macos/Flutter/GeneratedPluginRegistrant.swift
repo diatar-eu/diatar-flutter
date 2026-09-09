be88c77789523e0bc3bebb3da4259a46a8794400
Vörös László <theprogrambear@gmail.com>
2026-09-09 15:49:02 +0200

    Diavetítő fixálása

--- DiaVetito/macos/Flutter/GeneratedPluginRegistrant.swift
+++ DiaVetito/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,14 +5,20 @@
 import FlutterMacOS
 import Foundation
 
+import device_info_plus
+import irondash_engine_context
 import mobile_scanner
 import package_info_plus
 import shared_preferences_foundation
+import super_native_extensions
 import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  IrondashEngineContextPlugin.register(with: registry.registrar(forPlugin: "IrondashEngineContextPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SuperNativeExtensionsPlugin.register(with: registry.registrar(forPlugin: "SuperNativeExtensionsPlugin"))
   WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
